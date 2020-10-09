5447753052adf390b8ad36538d7a9572fecba5ca
Lukecp <lukecp@pm.me>
2020-10-08 23:07:46 -0400

    completed stretch goals

--- index.js
+++ index.js
@@ -156,6 +156,12 @@ class Instructor extends Lambdasian {
 	grade(student, subject) {
 		return `${student.name} receives a perfect score on ${subject}`;
 	}
+	
+	grader(student) {
+		const grd = Math.floor(Math.random() * 61) - 30;
+		student.grade += grd;
+    	student.graduate();
+	}
 }
 
 /*
@@ -179,20 +185,27 @@ class Student extends Lambdasian {
 		this.previousBackground = object.previousBackground;
 		this.className = object.className;
 		this.favSubjects = object.favSubjects;
+		this.grade = 50;
 	};
 	
 	listSubjects() {
 		return this.favSubjects;
-	}
+	};
 	
 	PRAssignment(subject) {
 		return `${this.name} has submitted a PR for ${subject}`;
-	}
+	};
 	
 	sprintChallenge(subject) {
-		return `${this.name} has begun sprint challenge on ${subject}`
-	}
-}
+		return `${this.name} has begun sprint challenge on ${subject}`;
+	};
+  
+  graduate() {
+    if (this.grade > 70) {
+      console.log(`${this.name} is ready to graduate!`);
+    };
+  };
+};
 
 /*
   TASK 6
@@ -226,7 +239,7 @@ class ProjectManager extends Instructor {
 /*
   STRETCH PROBLEM (no tests!)
     - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
-    - Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
+    - Now that our students have a grade, build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
     - Add a graduate method to a student.
       + This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
       + If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
